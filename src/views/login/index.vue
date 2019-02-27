396b863abd05b4518ee2bd0dac5228d7dee4a03b
selfconfidence <mynameisweiyan@gmail.com>
2019-02-27 10:31:13 +0800

    w

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,7 +18,7 @@
           name="password"
           auto-complete="on"
           placeholder="请输入密码"
-          @keyup.enter.native="handleLogin" />
+          @keyup.enter.native="handleLogin" /> 
         <span class="show-pwd" @click="showPwd">
           <svg-icon :icon-class="pwdType === 'password' ? 'eye' : 'eye-open'" />
         </span>
